645bb3202f8ce0296568520792b6c8b5c0db58a4
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-09 14:16:49 +0300

    describe group test

--- script.test.js
+++ script.test.js
@@ -7,7 +7,18 @@ it('is a silly test', () => {
   googleSearch('testTest', dbMock)
 })
 
-it('is searching google', () => {
-  expect(googleSearch('testTest', dbMock)).toEqual([])
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dog-pictures.com'])
+describe('googleSearch', () => {
+  it('is searching google', () => {
+    expect(googleSearch('testTest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dog-pictures.com'])
+  })
+
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
+  })
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3)
+  })
 })
