c1529e1a7f3d86065c3fd941e754ae383712f908
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-09 15:03:49 +0300

    async test

---
+++ script2.test.js
@@ -0,0 +1,16 @@
+const swapi = require('./script2')
+
+it('calls swapi to get people', () => {
+  expect.assertions(1)
+  return swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87)
+  })
+})
+
+it('calls swapi to get people with a promise', () => {
+  expect.assertions(2)
+  return swapi.getPeoplePromise(fetch).then(data => {
+    expect(data.count).toEqual(87)
+    expect(data.results.length).toBeGreaterThan(5)
+  })
+})
