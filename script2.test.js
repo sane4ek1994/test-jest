ce8724d6bb76e9e88600eba47cd816a897f9d764
Alexandr Naum <mfq07k9cz@gmail.com>
2023-01-10 14:14:31 +0300

    mocks and spies

--- script2.test.js
+++ script2.test.js
@@ -14,3 +14,23 @@ it('calls swapi to get people with a promise', () => {
     expect(data.results.length).toBeGreaterThan(5)
   })
 })
+
+it('get people returns count and resutls', () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  )
+
+  expect.assertions(4)
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1)
+    expect(mockFetch).toHaveBeenCalledWith('https://swapi.py4e.com/api/people/')
+    expect(data.count).toEqual(87)
+    expect(data.results.length).toBeGreaterThan(5)
+  })
+})
